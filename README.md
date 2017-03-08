9eb786d04564288ede1f0629d8b2f3dd2086ad54
Kai Yuan <thng.kaiyuan@gmail.com>
2017-03-09 02:07:03 +0800

    Initial and maybe last commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# About Secure Channel
+
+# Installing the Secure Channel Chrome Extension
+
+1. Clone this repository
+2. Go to `chrome://extensions`
+3. Ensure that "Developer mode" (on the top-right hand corner of the window) is checked
+4. Click on the "Load unpacked extension..." button
+5. Select the cloned directory
---
+++ background.js
@@ -0,0 +1,62 @@
+const UNSET = 0;
+const INSECURE = 1;
+const SECURE = 2;
+var currentTabId = null;
+var tabStatuses = {};
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+        for (var i = 0; i < details.responseHeaders.length; ++i) {
+            if (details.responseHeaders[i].name === 'x-authentication-results') {
+                if(tabStatuses[currentTabId] === UNSET) setSecureTab();
+                var results = details.responseHeaders[i].value;
+                var header_results = results.match(/header=([a-z]+)/);
+                var body_results = results.match(/body=([a-z]+)/);
+                if((header_results && header_results[1] != "pass") || (body_results && body_results[1] != "pass")) {
+                    setInsecureTab();
+                    console.log(details.url + " is insecure");
+                    return {cancel: true};;
+                } else {
+                    console.log(details.url + " is secure");
+                }
+            }
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking", "responseHeaders"]
+);
+
+chrome.webNavigation.onCommitted.addListener(function(info) {
+    resetTab();
+    currentTabId = info.tabId;
+});
+
+chrome.tabs.onActivated.addListener(function(tab) {
+    currentTabId = tab.tabId;
+});
+
+function setCurrentTabTitleAndIcon(title, icon) {
+    chrome.browserAction.setTitle({
+        title : title,
+        tabId: currentTabId
+    });
+    chrome.browserAction.setIcon({
+        path : icon,
+        tabId: currentTabId
+    });
+}
+
+function resetTab() {
+    tabStatuses[currentTabId] = UNSET;
+    setCurrentTabTitleAndIcon("Secure channel is not used", "icons/Security-icon.png");
+}
+
+function setInsecureTab() {
+    tabStatuses[currentTabId] = INSECURE;
+    setCurrentTabTitleAndIcon("Security of channel is in doubt", "icons/Security-Caution-icon.png");
+}
+
+function setSecureTab() {
+    tabStatuses[currentTabId] = SECURE;
+    setCurrentTabTitleAndIcon("Channel is secure", "icons/Security-Approved-icon.png");
+}
